95754164edde11786ceb4a5efac9a96085ca74df
ryanwarrick <ryanwarrick1@gmail.com>
2021-11-26 20:00:32 -0800

    Replace hardcoded values with config values.

--- zaphod/__init__.py
+++ zaphod/__init__.py
@@ -35,6 +35,16 @@ def create_app():
     @app.context_processor
     def inject_datetime_now():
         return {'now': datetime.utcnow()}
+    # For instance-specific footer info
+
+    @app.context_processor
+    def inject_website_author_name():
+        return {'website_author_name': app.config['WEBSITE_AUTHOR_NAME']}
+    # For instance-specific footer info
+
+    @app.context_processor
+    def inject_website_url():
+        return {'website_url': app.config['WEBSITE_URL']}
 
     # Initialize Flask-SQLAlchemy and the init-db commands
     from zaphod.models import db
--- zaphod/config.py
+++ zaphod/config.py
@@ -3,4 +3,6 @@
 # See README.md file for instructions to build instance-specific config file that overwrites the default values set below.
 
 SECRET_KEY = 'dev'  # Default value
+WEBSITE_AUTHOR_NAME = 'John Doe'
+WEBSITE_URL = 'https://example.com/'
 RESUME_FILE_NAME = 'Resume.pdf'
